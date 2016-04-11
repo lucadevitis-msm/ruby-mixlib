76e26144951a0a07b8a4961b69e66e03f2ccbd74
Luca De Vitis <luca.devitis@moneysupermarket.com>
2016-04-11 13:54:40 +0100

    Rubocop cleanup

--- lib/mixlib/facter.rb
+++ lib/mixlib/facter.rb
@@ -1,5 +1,6 @@
 require 'facter'
 
+# rubocop:disable Style/Documentation
 module Mixlib
   module Facter
     def facter
@@ -9,6 +10,6 @@ module Mixlib
 end
 class Hash
   def to_hiera_scope
-    self.map {|fact, value| ["::#{fact}", value]}.to_h
+    map { |fact, value| ["::#{fact}", value] }.to_h
   end
 end
