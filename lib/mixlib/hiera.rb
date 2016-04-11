795cde27da589db33f56c33496f6cef395a4db34
Luca De Vitis <luca.devitis@moneysupermarket.com>
2016-04-11 15:06:55 +0100

    Code cleanup

--- lib/mixlib/hiera.rb
+++ lib/mixlib/hiera.rb
@@ -6,24 +6,25 @@ module Mixlib
   module Hiera
     include Mixlib::Facter
 
-    def hiera_config(options = nil)
-      options ||= {}
+    def hiera_config(options = {})
       file = options[:config_file] || '/etc/hiera.yaml'
       (@hiera_config ||= {})[file] ||= YAML.load_file(file).freeze
       (options[:config_override] || {}).dup.update @hiera_config[file]
     end
 
-    def hiera_instance(options = nil)
+    def hiera_instance(options = {})
       config = hiera_config options
       (@hiera_instance ||= {})[config.hash] ||= Hiera.new(config: config)
     end
 
-    def hiera_lookup(key, options = nil)
-      hiera_instance.lookup key,
-                            options[:default],
-                            options[:scope] || facter.to_hiera_scope,
-                            options[:order_override],
-                            options[:resolution_type]
+    def hiera_lookup(key, options = {})
+      hiera = hiera_instance config_file: options[:config_file]
+                             config_override: options[:config_override]
+      hiera.lookup key,
+                   options[:default],
+                   options[:scope] || facter.to_hiera_scope,
+                   options[:order_override],
+                   options[:resolution_type]
     end
   end
 end
